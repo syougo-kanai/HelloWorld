b4381994d9af499219d2d23168a2c823b974835b
taro.kagayaki <snapwolz@gmail.com>
2023-12-07 17:23:54 +0900

    Initial Commit

---
+++ HollwWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HollwWorld
+//
+//  Created by MTBS049 on 2023/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
